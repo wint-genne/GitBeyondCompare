74c68fc1d6f1f37a00ec1b02378582bd6e0d2ce2
Christian Genne <christian.genne@wint.se>
2013-02-04 09:41:34 +0100

    Initial commit

---
+++ GitBeyondCompare/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GitBeyondCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = args[0];
+            var target = args[1];
+
+            Console.WriteLine(source + " " + target);
+
+            if (File.Exists(source) && IsTemp(source))
+            {
+                var temp = Path.GetTempFileName();
+                File.Copy(source, temp, true);
+                source = temp;
+            }
+            if (File.Exists(target) && IsTemp(target))
+            {
+                var temp = Path.GetTempFileName();
+                File.Copy(target, temp, true);
+                target = temp;
+            }
+
+            Process.Start(@"c:\Program Files (x86)\Beyond Compare 3\bcomp.exe", source + " " + target);
+        }
+
+        private static bool IsTemp(string source)
+        {
+            Console.WriteLine(Path.GetDirectoryName(source).ToLower() + " == " + Path.GetTempPath().ToLower());
+            return Path.GetDirectoryName(source).ToLower() + "\\" == Path.GetTempPath().ToLower();
+        }
+    }
+}
